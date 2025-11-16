390c35dd90c1aecb2fa15c286ffd2267176876fb
nirmalprasadk <nirmal.prasad@solitontech.com>
2025-11-16 16:05:24 +0530

    fixed cluedle

--- Bots/Bots/CluedleBot.cs
+++ Bots/Bots/CluedleBot.cs
@@ -1,11 +1,35 @@
 ﻿using Reusables.Contracts;
+using Reusables.Models.SBoxMessage;
 
 namespace Bots.Bots;
 
 public class CluedleBot(ISboxClient sBoxClient, IAIService aIService, string? name = null) : BotBase(sBoxClient, aIService, name)
 {
-    protected override void OnSBoxMessageReceived(string message)
+    protected override void HandleAck(AckMessage ack)
     {
-        throw new NotImplementedException();
+        Console.WriteLine($"Received Ack for: {ack.AckFor}");
+    }
+
+    protected override void HandleCommand(CommandMessage command)
+    {
+        IBot bot = this;
+
+        BotGuessMessage botGuessMessage = new()
+        {
+            MatchId = command.MatchId,
+            GameId = command.GameId,
+            Otp = command.Otp,
+            Guess = "APPLE"
+        };
+
+        bot.SendMessageToSBox(botGuessMessage);
+    }
+
+    protected override void HandleGameResult(GameResultMessage gameResult)
+    {
+        Console.WriteLine($" Result for match {gameResult.MatchId}");
+        Console.WriteLine($" Game:     {gameResult.GameId}");
+        Console.WriteLine($" Outcome:  {gameResult.Result?.ToUpper()}");
+        Console.WriteLine($" Word:     {gameResult.Word}");
     }
 }
